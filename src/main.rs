a204d37ce6dbddb4c5937260d026232e68d12b66
donkomura <koiru3822fs@gmail.com>
2022-08-25 15:17:32 +0900

    accept Set/Get redis commands and store data into HashMap

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+use tokio::net::{TcpListener, TcpStream};
+use mini_redis::{Connection, Frame};
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    loop {
+        //  (TcpStream, ip/port info)
+        let (socket, _) = listener.accept().await.unwrap();
+
+        // spawn new tasks for each socket
+        tokio::spawn(async move {
+            process(socket).await;
+        });
+    }
+}
+
+async fn process(socket: TcpStream) {
+    use mini_redis::Command::{self, Get, Set};
+    use std::collections::HashMap;
+
+    // create hash map to store data
+    let mut db = HashMap::new();
+
+    // handle as Redis Frame
+    let mut connection = Connection::new(socket);
+
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    // Bulk needs `Bytes`
+                    // so, we convert `&Vec<u8>` into `Bytes` here
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
+
+        connection.write_frame(&response).await.unwrap();
+    }
+}
